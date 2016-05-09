cc61bd47f1b36ec518833e58a5b17b597aa7dddd
yue wang <wangyue@iot.chinamobile.com>
2016-05-09 14:37:36 +0800

    all

---
+++ DailyBuild/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DailyBuild
+//
+//  Created by yue wang on 16/5/9.
+//  Copyright © 2016年 cmcc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DailyBuild/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  DailyBuild
+//
+//  Created by yue wang on 16/5/9.
+//  Copyright © 2016年 cmcc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor yellowColor];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
